6db0b7a19202754a814a2cc6cf5f5f8b847764de
rias-z <chestermx88@gmail.com>
2017-12-04 01:13:37 +0900

    [API-3] Admin画面からItemの編集（新規追加）

--- dbshive_api/views/api_item.py
+++ dbshive_api/views/api_item.py
@@ -1,6 +1,8 @@
-from flask import make_response, jsonify
-from dbshive_api import app
+from flask import make_response, jsonify, request
+from dbshive_api import app, db
+from dbshive_api.models.user import User
 from dbshive_api.models.item import Item
+import json
 
 endpoint = '/items'
 
@@ -13,4 +15,28 @@ def get_item():
     for item in items:
         results.append(item.to_json())
 
+    return make_response(jsonify(results))
+
+
+@app.route('%s' % endpoint, methods=['POST'])
+def post_item():
+    data = json.loads(request.data.decode('utf-8'))
+
+    new_item = Item(
+        name=data.get('name'),
+        description=data.get('description'),
+    )
+
+    user_list = data.get('developers')
+    users = User.query.filter(User.id.in_(user_list)).all()
+    for user in users:
+        new_item.developer.append(user)
+
+    db.session.add(new_item)
+    db.session.commit()
+
+    items = Item.query.all()
+    results = []
+    for item in items:
+        results.append(item.to_json())
     return make_response(jsonify(results))
